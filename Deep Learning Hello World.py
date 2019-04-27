8c7be1fbf809a61b88e4fcce82dd4577905859e3
jxcjean <jxcjean@163.com>
2019-04-27 22:04:52 +0800

    first commit

---
+++ Deep Learning Hello World.py
@@ -0,0 +1,21 @@
+# Intel MKL FATAL ERROR: Cannot load mkl_intel_thread.dll.
+#### https://blog.csdn.net/zhangpeterx/article/details/84872125
+
+def helloWorld(object):
+    print('*********Start ' + object + '*********')
+
+helloWorld("Deep Learning")
+
+#import cv2
+import numpy as np
+#import matplotlib
+
+np_a=np.array([[1,2],[3,4]])
+np_b=np.array([[1,3],[2,4]])
+print(np_a+np_b)
+print(np_a[1])
+np_a=np_a.flatten()
+print(np_a)
+
+
+
