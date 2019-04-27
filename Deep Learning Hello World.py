d0f65dcaba2b11c83cf670c8ddf506779c7d3954
jxcjean <jxcjean@163.com>
2019-04-27 22:08:18 +0800

    hello

--- Deep Learning Hello World.py
+++ Deep Learning Hello World.py
@@ -8,7 +8,7 @@ helloWorld("Deep Learning")
 
 #import cv2
 import numpy as np
-#import matplotlib
+import matplotlib.pyplot as plt
 
 np_a=np.array([[1,2],[3,4]])
 np_b=np.array([[1,3],[2,4]])
@@ -17,5 +17,10 @@ print(np_a[1])
 np_a=np_a.flatten()
 print(np_a)
 
+x=np.arange(0,6,0.1) #以0.1位单位，生成0-6的数据
+y=np.sin(x)
 
+print(y)
+plt.plot(x,y)
+plt.show()
 
